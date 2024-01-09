d91c0e5bbbbf03bab308f1b4286a65ede5c3a6cd
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2024-01-09 21:00:00 +0530

    designing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     return (
@@ -7,7 +7,7 @@ export default function Login() {
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <img src="" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                        <img src="" alt="Logo" style={{marginRight:`6px`}} width="30" height="24" className="d-inline-block align-text-top" />
                         Bootstrap
                     </a>
                 </div>
