7fc550778c25737847a3fd4864abe4b4c9eb1bad
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2024-01-14 00:28:35 +0530

    changing firebase and feching data

--- src/Context/Data/Datastate.js
+++ src/Context/Data/Datastate.js
@@ -1,39 +1,72 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { child, get, getDatabase, ref } from 'firebase/database';
 
 
 const DataContext = createContext();
 
 export function useGlobalData() {
-  return useContext(DataContext);
+    return useContext(DataContext);
 }
 
 const DataState = (props) => {
 
-  const showAlert = (device, message) => {
-    // console.log({ device, message });
-    // if (sameAlert) {
-    //   return;
-    // }
-    // setSameAlert(true);
-    const path = window.location.pathname;
-    if (path === '/login' || path === '/signup' || path === '/') {
-      // console.log(path);
-      return;
-    }
-    else {
-      toast.error(`${device} ${message}`);
-      return;
-    }
-  };
+    const showAlert = (device, message) => {
+        // console.log({ device, message });
+        // if (sameAlert) {
+        //   return;
+        // }
+        // setSameAlert(true);
+        const path = window.location.pathname;
+        if (path === '/login' || path === '/signup' || path === '/') {
+            // console.log(path);
+            return;
+        }
+        else {
+            toast.error(`${device} ${message}`);
+            return;
+        }
+    };
+    // eslint-disable-next-line
+    const [detection, setDetection] = useState("");
 
-  const state = {
-    showAlert
-  };
+    function readData(dir, stateName) {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `${dir}/`))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    console.log(snapshot.val());
 
-  return (
-    <DataContext.Provider value={state}>{props.children}</DataContext.Provider>
-  );
+                    stateName(snapshot.val());
+                    // showAlert(collection,  dir);
+                } else {
+                    console.log('No data available');
+                }
+            })
+            .then(() => {
+                console.log('Loading Done');
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+
+    useEffect(() => {
+        readData('detection_results', setDetection);
+        // console.log(JSON.stringify(detection));
+    }, [])
+
+
+
+
+    const state = {
+        showAlert
+    };
+
+    return (
+        <DataContext.Provider value={state}>{props.children}</DataContext.Provider>
+    );
 };
 
 export default DataState;
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,20 +1,31 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
+import { getStorage, ref } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore/lite';
+import 'firebase/storage';
+
+// import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2VphySRT4jc2lMSY23AtOr_cbbVz-7dg",
-  authDomain: "aarakshak-monitoringsystem.firebaseapp.com",
-  projectId: "aarakshak-monitoringsystem",
-  storageBucket: "aarakshak-monitoringsystem.appspot.com",
-  messagingSenderId: "116370673853",
-  appId: "1:116370673853:web:4087c128845f759442e7d4",
-  measurementId: "G-K4PVC2W4RS"
+  apiKey: "AIzaSyAIErp6lsivbbzLkfLs1Hi-1_BHt3v6OFs",
+  authDomain: "object-detection-4b3de.firebaseapp.com",
+  databaseURL: "https://object-detection-4b3de-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "object-detection-4b3de",
+  storageBucket: "object-detection-4b3de.appspot.com",
+  messagingSenderId: "595470443083",
+  appId: "1:595470443083:web:a0763a88bee3d31f6d23ce",
+  measurementId: "G-PTKQ2H406Q"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const database = getDatabase(app);
+const db = getFirestore(app); // Initialize Firestore
+const storage = getStorage();
+const storageRef = ref;
 
-export { app, analytics, auth };
+export { app, analytics, auth, database, db, storage, storageRef };
