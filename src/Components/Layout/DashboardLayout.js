e5f5231f4dd98c31b1dd267dd5875314b31195d9
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2024-01-13 16:28:07 +0530

    fixing minor bugs

--- src/Components/Layout/DashboardLayout.js
+++ src/Components/Layout/DashboardLayout.js
@@ -11,23 +11,23 @@ export default function DashboardLayout() {
                     <div className="card-body bg-dark">
                         <h5 className="card-title" style={{ color: `white` }}>Zonal Cam - 1</h5>
                         <p className="card-text">
-                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone1" aria-controls="offcanvasWithBothOptions">
+                            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone1" aria-controls="offcanvasWithBothOptions">
                                 See More Details
                             </button>
                         </p>
                         {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
                     </div>
-                    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasZone1" aria-labelledby="offcanvasWithBothOptionsLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 1 Camera</h5>
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasZone1" aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 1 Camera</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
+                        <div className="offcanvas-body">
                             <img src={img} alt="" className='canvas-cam' />
-                            <p>
+                            <div>
                                 <p>Location : <span></span></p>
                                 <p>Total Triggers : <span></span></p>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,23 +38,23 @@ export default function DashboardLayout() {
                     <div className="card-body bg-dark">
                         <h5 className="card-title" style={{ color: `white` }}>Zonal Cam - 2</h5>
                         <p className="card-text">
-                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone2" aria-controls="offcanvasWithBothOptions">
+                            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone2" aria-controls="offcanvasWithBothOptions">
                                 See More Details
                             </button>
                         </p>
                         {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
                     </div>
-                    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasZone2" aria-labelledby="offcanvasWithBothOptionsLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 2 Camera</h5>
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasZone2" aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 2 Camera</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
+                        <div className="offcanvas-body">
                             <img src={img} alt="" className='canvas-cam' />
-                            <p>
+                            <div>
                                 <p>Location : <span></span></p>
                                 <p>Total Triggers : <span></span></p>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -65,23 +65,23 @@ export default function DashboardLayout() {
                     <div className="card-body bg-dark">
                         <h5 className="card-title" style={{ color: `white` }}>Zonal Cam - 3</h5>
                         <p className="card-text">
-                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone3" aria-controls="offcanvasWithBothOptions">
+                            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone3" aria-controls="offcanvasWithBothOptions">
                                 See More Details
                             </button>
                         </p>
                         {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
                     </div>
-                    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasZone3" aria-labelledby="offcanvasWithBothOptionsLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 3 Camera</h5>
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasZone3" aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 3 Camera</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
+                        <div className="offcanvas-body">
                             <img src={img} alt="" className='canvas-cam' />
-                            <p>
+                            <div>
                                 <p>Location : <span></span></p>
                                 <p>Total Triggers : <span></span></p>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -92,23 +92,23 @@ export default function DashboardLayout() {
                     <div className="card-body bg-dark">
                         <h5 className="card-title" style={{ color: `white` }}>Zonal Cam - 4</h5>
                         <p className="card-text">
-                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone4" aria-controls="offcanvasWithBothOptions">
+                            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasZone4" aria-controls="offcanvasWithBothOptions">
                                 See More Details
                             </button>
                         </p>
                         {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
                     </div>
-                    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasZone4" aria-labelledby="offcanvasWithBothOptionsLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 4 Camera</h5>
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasZone4" aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Zonal - 4 Camera</h5>
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
+                        <div className="offcanvas-body">
                             <img src={img} alt="" className='canvas-cam' />
-                            <p>
+                            <div>
                                 <p>Location : <span></span></p>
                                 <p>Total Triggers : <span></span></p>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
