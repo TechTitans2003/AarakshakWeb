86882da33afd8488e4d2f35f5865249bcde62ead
YashashavGoyal <76003258+YashashavGoyal@users.noreply.github.com>
2024-03-27 01:19:59 +0530

    updating login ui

--- src/Components/Layout/InputForm.js
+++ src/Components/Layout/InputForm.js
@@ -7,30 +7,37 @@ export default function Login(props) {
 
     return (
         <>
-            <nav className="navbar navbar-dark bg-dark">
+            {/* <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
                         <img src={props.logo} alt="Logo" width="20" height="24" className="d-inline-block align-text-top logo-align" />
                         <span style={{ marginLeft: `34px` }}>{props.title}</span>
                     </a>
                 </div>
-            </nav>
+            </nav> */}
+
+
+            <div className="nav-shape center">
+                <img src={props.logo} alt="" />
+                <h2>{props.title}</h2>
+            </div>
+
             <div className="l-form">
-        <div className="shape1"></div>
-        <div className="shape2"></div>
+                <div className="shape1"></div>
+                <div className="shape2"></div>
 
-        <div className="form">
-            {/* <img src="assets/img/authentication.svg" alt="" className="form__img"> */}
-            <img src={authBg} alt="" className="form__img" />
+                <div className="form">
+                    {/* <img src="assets/img/authentication.svg" alt="" className="form__img"> */}
+                    <img src={authBg} alt="" className="form__img" />
 
-            <form action="" className="form__content">
-                <Outlet/>
-            </form>
+                    <form action="" className="form__content">
+                        <Outlet />
+                    </form>
 
 
-        </div>
+                </div>
 
-    </div>
+            </div>
 
         </>
     )
